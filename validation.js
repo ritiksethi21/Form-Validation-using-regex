7c8c21093836965906bd6e742209663bdb35a2c7
Ritik Sethi <58940080+ritiksethi21@users.noreply.github.com>
2021-02-22 20:15:16 +0530

    Create validation.js

---
+++ validation.js
@@ -0,0 +1,30 @@
+const inputs = document.querySelectorAll('input');
+
+// regex patterns
+const patterns = {
+        telephone: /^[^0]\d{9}$/,
+        username: /^[a-z\d]{5,12}$/i,
+        password: /^[\d\w@-]{8,20}$/i,
+        slug: /^[a-z\d-]{8,20}$/,
+        email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+        //             yourname @ domain   .  com          ( .uk )
+};
+
+// validation function
+function validate(field, regex){
+
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+
+}
+
+// attach keyup events to inputs
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+            // console.log(patterns[e.target.attributes.name.value]);
+            validate(e.target, patterns[e.target.attributes.name.value]);
+    });
+});
